4b1f384b11eee3bf30f945d4a5695dc6b58ea5b3
phatak-dev <phatak.dev@gmail.com>
2020-03-31 17:49:27 +0530

    added combined source of batch and streaming

--- src/main/scala/com/madhukaraphatak/spark/sources/datasourcev2/streamandbatch/DataSourceV2StreamAndBatchExample.scala
+++ src/main/scala/com/madhukaraphatak/spark/sources/datasourcev2/streamandbatch/DataSourceV2StreamAndBatchExample.scala
@@ -1,21 +1,29 @@
-package com.madhukaraphatak.spark.sources.datasourcev2.streaming
+package com.madhukaraphatak.spark.sources.datasourcev2.streamandbatch
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
 
-object DataSourceV2StreamingExample {
+object DataSourceV2StreamAndBatchExample {
   def main(args: Array[String]): Unit = {
     val sparkSession = SparkSession.builder.
           master("local[2]")
           .appName("streaming example")
           .getOrCreate()
 
-    val streamingDf = sparkSession.
-      readStream.
-      format("com.madhukaraphatak.spark.sources.datasourcev2.streaming.simple")
+
+    val dataSource = "com.madhukaraphatak.spark.sources.datasourcev2.streamandbatch.simple"
+
+    val batchDf = sparkSession
+      .read
+      .format(dataSource)
       .load()
 
+    batchDf.show()
 
+    val streamingDf = sparkSession.
+      readStream.
+      format(dataSource)
+      .load()
 
     val query = streamingDf.writeStream
       .format("console")
